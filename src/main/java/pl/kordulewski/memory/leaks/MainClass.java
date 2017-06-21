dd1e0b56ad2fefaac0c13b07629fe096e820e472
Michał Kordulewski <michal@kordulewski.pl>
2017-06-21 18:44:09 +0200

    main class for jar

---
+++ src/main/java/pl/kordulewski/memory/leaks/MainClass.java
@@ -0,0 +1,12 @@
+package pl.kordulewski.memory.leaks;
+
+/**
+ * Created by Michał Kordulewski on 21.06.17.
+ */
+public class MainClass {
+
+    public static void main(String... args) {
+        // TODO
+    }
+
+}
