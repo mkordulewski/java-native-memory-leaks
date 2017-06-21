e87499eaba3f5d79c29022090d951754af14c78a
Michał Kordulewski <michal@kordulewski.pl>
2017-06-21 18:48:10 +0200

    main class prints message from file

--- src/main/java/pl/kordulewski/memory/leaks/MainClass.java
+++ src/main/java/pl/kordulewski/memory/leaks/MainClass.java
@@ -1,12 +1,48 @@
 package pl.kordulewski.memory.leaks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by Michał Kordulewski on 21.06.17.
  */
 public class MainClass {
 
     public static void main(String... args) {
-        // TODO
+        InputStream input = "".getClass().getResourceAsStream("/info.txt");
+        String result = getStringFromInputStream(input);
+        System.out.println(result);
+        try {
+            input.close();
+        } catch (IOException e) {
+            System.out.println("Unknown error");
+        }
+    }
+
+    private static String getStringFromInputStream(InputStream is) {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Unknown error");
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println("Unknown error");
+                }
+            }
+        }
+        return sb.toString();
     }
 
 }
